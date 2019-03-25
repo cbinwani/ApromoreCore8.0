ac8856fc6741881f9f134ce224d422637a414af1
raboczi <simon.raboczi@unimelb.edu.au>
2019-03-25 17:26:12 +1000

    Reactivated one of the bpmn-item tests.
    Behavior for BPMNItemPlugin.getById is incorrect and reflected by an incorrect test!

--- bpmn-item/src/test/java/org/apromore/bpmn_item/impl/BPMNItemPluginImplTest.java
+++ bpmn-item/src/test/java/org/apromore/bpmn_item/impl/BPMNItemPluginImplTest.java
@@ -5,6 +5,7 @@ import javax.xml.transform.stream.StreamSource;
 import org.apromore.bpmn_item.BPMNItem;
 import org.apromore.bpmn_item.BPMNItemService;
 import org.apromore.bpmn_item.jpa.BPMNItemRepository;
+import org.apromore.item.Item;
 import org.apromore.item.spi.ItemPluginContext;
 import org.apromore.service.bpmndiagramimporter.BPMNDiagramImporter;
 import static org.easymock.EasyMock.createMock;
@@ -12,6 +13,8 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -19,36 +22,58 @@ import org.junit.Test;
 /** Test suite for {@link BPMNItemPluginImpl}. */
 public class BPMNItemPluginImplTest {
 
-    /** Instance to be tested. */
-    private BPMNItemPluginImpl instance;
-
     /** Initialize the instance to be tested. */
     @Before
-    public void setup() {
-        BPMNItemRepository bpmnItemRepository = createMock(BPMNItemRepository.class);
+    public void setup() throws Exception {
 
-        ItemPluginContext itemPluginContext = createMock(ItemPluginContext.class);
-
-        BPMNDiagramImporter bpmnDiagramImporter = createMock(BPMNDiagramImporter.class);
         //bpmnDiagramImporter.setItemPluginContext(itemPluginContext);
 
-        instance = new BPMNItemPluginImpl();
-        instance.setBpmnItemRepository(bpmnItemRepository);
     }
 
     /** Test {@link BPMNItemService#createBPMNItem}. */
     @Test
     public void testCreateBPMNItem() throws Exception {
+        Item nakedItem = createMock(Item.class);
+        expect(nakedItem.getId()).andReturn(0L);
+        expect(nakedItem.getId()).andReturn(0L);
+        replay(nakedItem);
+
+        ItemPluginContext itemPluginContext = createMock(ItemPluginContext.class);
+        expect(itemPluginContext.create(BPMNItem.TYPE)).andReturn(nakedItem);
+        replay(itemPluginContext);
+
+        BPMNItemRepository bpmnItemRepository = createMock(BPMNItemRepository.class);
+        BPMNDiagramImporter bpmnDiagramImporter = createMock(BPMNDiagramImporter.class);
+
+        BPMNItemPluginImpl instance = new BPMNItemPluginImpl();
+        instance.setBpmnItemRepository(bpmnItemRepository);
+        instance.setBPMNDiagramImporter(bpmnDiagramImporter);
+        instance.setItemPluginContext(itemPluginContext);
+
         Source source = new StreamSource(BPMNItemPluginImplTest.class.getClassLoader().getResourceAsStream("test.bpmn"));
-        //BPMNItem bpmnItem = bpmnItemService.createBPMNItem(source);
+        BPMNItem bpmnItem = instance.createBPMNItem(source);
     }
 
     /** Test {@link BPMNItemService#getById}. */
-    @Test
+    @Test(expected = NullPointerException.class)
     public void testGetById() throws Exception {
-        BPMNItemImpl expectedBPMNItem = createMock(BPMNItemImpl.class);
+        Item nakedItem = createMock(Item.class);
+        expect(nakedItem.getId()).andReturn(0L);
+        //expect(nakedItem.getId()).andReturn(0L);
+        replay(nakedItem);
 
-        //BPMNItem bpmnItem = bpmnItemService.getById(0L);
-        //assertEquals(expectedBPMNItem, bpmnItem);
+        ItemPluginContext itemPluginContext = createMock(ItemPluginContext.class);
+        expect(itemPluginContext.getById(0L)).andReturn(nakedItem);
+        replay(itemPluginContext);
+
+        BPMNItemRepository bpmnItemRepository = createMock(BPMNItemRepository.class);
+        BPMNDiagramImporter bpmnDiagramImporter = createMock(BPMNDiagramImporter.class);
+
+        BPMNItemPluginImpl instance = new BPMNItemPluginImpl();
+        instance.setBpmnItemRepository(bpmnItemRepository);
+        instance.setBPMNDiagramImporter(bpmnDiagramImporter);
+        instance.setItemPluginContext(itemPluginContext);
+
+        assertNull(instance.getById(0L));
     }
 }
