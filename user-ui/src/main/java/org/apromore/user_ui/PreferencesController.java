094a07916807cd0ec5b7d56fca69cc4249b410c4
raboczi <raboczi@icloud.com>
2019-05-22 12:02:10 +1000

    Added Account/Preferences... menu item.
    This currently isn't functional, but it's prototyping better Karaf/ZK integration.

---
+++ user-ui/src/main/java/org/apromore/user_ui/PreferencesController.java
@@ -0,0 +1,64 @@
+package org.apromore.user_ui;
+
+/*-
+ * #%L
+ * Apromore :: user-ui
+ * %%
+ * Copyright (C) 2019 The Apromore Initiative
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+//import java.util.Properties;
+import java.util.ResourceBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.util.Locales;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Window;
+
+/**
+ * Controller for <code>preferences.zul</code>.
+ */
+public class PreferencesController extends SelectorComposer<Component> {
+
+    /** Logger.  Named after the class. */
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(PreferencesController.class);
+
+    /** @return localized text catalogue */
+    public ResourceBundle getLabels() {
+        return ResourceBundle.getBundle("Labels", Locales.getCurrent());
+    }
+
+    /** Window. */
+    @Wire private Window win;
+
+    /** The current ZK theme. */
+    @Wire("#themeListbox")
+    private Listbox themeListbox;
+
+    /** @param event  button click */
+    @Listen("onClick = #okButton")
+    public void onClickOk(final Event event) {
+        win.detach();
+    }
+}
---
+++ user-ui/src/main/java/org/apromore/user_ui/PreferencesUIPlugin.java
@@ -0,0 +1,89 @@
+package org.apromore.user_ui;
+
+/*-
+ * #%L
+ * Apromore :: user-ui
+ * %%
+ * Copyright (C) 2019 The Apromore Initiative
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+import org.apromore.ui.spi.AbstractUIPlugin;
+import org.apromore.ui.spi.UIPlugin;
+import org.apromore.ui.spi.UIPluginContext;
+import org.apromore.user.Users;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zul.Window;
+
+/**
+ * {@link UIPlugin} for the Account/Preferences command.
+ */
+@Component(service = {UIPlugin.class})
+public final class PreferencesUIPlugin extends AbstractUIPlugin {
+
+    /** Logger.  Named after the class. */
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(PreferencesUIPlugin.class);
+
+    /** Sole constructor. */
+    public PreferencesUIPlugin() {
+        super("account.group",
+              "preferences.label",
+              "preferences.iconSclass");
+    }
+
+    /** {@inheritDoc}
+     *
+     * This implementation is enabled while the user session is authorized.
+     */
+    @Override
+    public boolean isEnabled(final UIPluginContext context) {
+        return Users.getUser() != null;
+    }
+
+    /** {@inheritDoc}
+     *
+     * This implementation presents the preferences pane.
+     */
+    @Override
+    public void execute(final UIPluginContext context) {
+        Window window;
+        try {
+            Reader reader = new InputStreamReader(
+                PreferencesUIPlugin.class
+                    .getClassLoader()
+                    .getResourceAsStream("zul/preferences.zul"),
+                "UTF-8"
+            );
+            Properties properties = new Properties();
+            properties.setProperty("test", "Test value");
+            window = (Window) Executions.createComponentsDirectly(reader, "zul",
+                context.getParentComponent(), properties);
+
+        } catch (IOException e) {
+            throw new Error("ZUL resource login.zul could not be created as as "
+                + "ZK component", e);
+        }
+    }
+}
